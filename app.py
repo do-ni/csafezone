3502fe6d856a9d28204a752fc2a0c6706cc9cb8b
the don king kong <106042806+do-ni@users.noreply.github.com>
2024-04-25 08:49:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,117 +1,56 @@
-from pywebio.input import *
-from pywebio.output import *
-
-import xlsxwriter
-import math
+import streamlit as st
 import pandas as pd
+import math
 
-from pywebio import config
-from pywebio import start_server
-from pywebio.session import run_js
-
-from pywebio.platform.flask import webio_view
-from pywebio import STATIC_PATH
-from flask import Flask, send_from_directory
-import argparse
-
-app = Flask(__name__)
-
-@config(theme="dark")
-
-
-def app2():
+def main():
     """
     C-Safe Zone
     """
-    put_text("C-Safe Zone").style('text-align:center; color: red; font-size: 40px'),
-    put_markdown("## Mencegah Pasang Baru Di Lokasi Bermasalah").style('text-align:center; color:Grey; font-size: 20px'),
+    st.title("C-Safe Zone")
+    st.markdown("## Mencegah Pasang Baru Di Lokasi Bermasalah")
 
     inputan()
-    
+
 def inputan():
-    file=input_group(
-        'Cek Data Pasang Baru dan TO Persil',[
-            file_upload('Data PB :',name='pb', accept='.csv', multiple=False),
-            file_upload('Data Persil :',name='persil', accept='.csv', multiple=False),
-            input('Input radius periksa (m)', name='jarak2', type=FLOAT)
-         ])
+    pb_file = st.file_uploader("Upload Data PB (CSV)", type=["csv"])
+    persil_file = st.file_uploader("Upload Data Persil (CSV)", type=["csv"])
+    jarak = st.number_input("Input radius periksa (m)")
 
-    open(str(file['pb']['filename']), 'wb').write(file['pb']['content'])
-    data_pb = pd.read_csv(str(file['pb']['filename']))
-    
-    open(str(file['persil']['filename']), 'wb').write(file['persil']['content'])
-    data_persil = pd.read_csv(str(file['persil']['filename']))
+    if pb_file and persil_file:
+        data_pb = pd.read_csv(pb_file)
+        data_persil = pd.read_csv(persil_file)
 
-    jml_data_persil=data_persil.index
-    jml_data_pb=data_pb.index
+        hitungan(data_pb, data_persil, jarak)
 
-    inpradius=file['jarak2']
-
-    hitungan(jml_data_persil, jml_data_pb, data_persil, data_pb, inpradius)
-
-def hitungan(jml_data_persil, jml_data_pb, data_persil, data_pb, inpradius):
-    
+def hitungan(data_pb, data_persil, inpradius):
     hasil = []
-    for i in jml_data_persil:
-        data = {}
-        data['IDPelTO'] = data_persil['IDPel'][i]
-        data['NamaTO'] = data_persil['Nama'][i]
-        data['TarifTO'] = data_persil['Tarif'][i]
-        data['DayaTO'] = data_persil['Daya'][i]
-        data['TanggalTO'] = data_persil['Tanggal'][i]
-        data['Koordinat'] = data_persil['Koordinat'][i]
-        data['Keterangan'] = data_persil['Temuan'][i]
 
-        IDPelTO = data['IDPelTO']
-        NamaTO = data['NamaTO']
-        TarifTO = data['TarifTO']
-        DayaTO = data['DayaTO']
-        TanggalTO = data['TanggalTO']
-        KoordinatTO = data['Koordinat']
-        Keterangan = data['Keterangan']
-    
-        koordinat=data['Koordinat']
-        x,y=koordinat.split(',')[0],koordinat.split(',')[1]
-        long=float(x)
-        lat=float(y)
+    for i, persil_row in data_persil.iterrows():
+        for j, pb_row in data_pb.iterrows():
+            koordinat = persil_row['Koordinat'].split(',')
+            koordinat2 = pb_row['Lokasi'].split(',')
 
-        for j in jml_data_pb:
-            data2 = {}
-            data2['IDPelPB'] = data_pb['IDPel'][j]
-            data2['NamaPB'] = data_pb['Nama'][j]
-            data2['TanggalPB'] = data_pb['Tanggal Pasang'][j]
-            data2['Koordinat2'] = data_pb['Lokasi'][j]
+            long, lat = float(koordinat[0]), float(koordinat[1])
+            long1, lat1 = float(koordinat2[0]), float(koordinat2[1])
 
-            IDPelPB = data2['IDPelPB']
-            NamaPB = data2['NamaPB']
-            TanggalPB = data2['TanggalPB']
-            KoordinatPB = data2['Koordinat2']
-
-            koordinat2=data_pb['Lokasi'][j]
-            x1,y1=koordinat2.split(',')[0],koordinat2.split(',')[1]
-            long1=float(x1)
-            lat1=float(y1)
-
-            jarak=round((math.sqrt(((long1-long)**2)+((lat1-lat)**2)))*110567)
+            jarak = round(math.sqrt(((long1 - long) ** 2) + ((lat1 - lat) ** 2)) * 110567)
 
             if jarak <= inpradius:
-                
-                row = [IDPelPB, NamaPB, TanggalPB, KoordinatPB, IDPelTO, NamaTO, TarifTO, DayaTO, TanggalTO, KoordinatTO, Keterangan, jarak]
+                row = [
+                    pb_row['IDPel'], pb_row['Nama'], pb_row['Tanggal Pasang'], pb_row['Lokasi'],
+                    persil_row['IDPel'], persil_row['Nama'], persil_row['Tarif'], persil_row['Daya'],
+                    persil_row['Tanggal'], persil_row['Koordinat'], persil_row['Temuan'], jarak
+                ]
                 hasil.append(row)
-    
 
-    put_table(hasil, header=['IDPel PB', 'Nama PB', 'Tanggal Pasang','Koordinat PB', 'IDPel TO', 'Nama TO', 'Tarif TO', 'Daya TO', 'Tanggal Temuan', 'Koordinat TO', 'Keterangan TO', 'Jarak (m)'])
-
-    put_button("Kembali",onclick=lambda: run_js('window.location.reload()'))
-
-#if __name__ == '__main__':
-#    start_server(app, port=80)
-app.add_url_rule('/tool', 'webio_view', webio_view(app2),
-		 methods=['GET', 'POST', 'OPTIONS'])
+    if hasil:
+        st.write(pd.DataFrame(hasil, columns=[
+            'IDPel PB', 'Nama PB', 'Tanggal Pasang', 'Koordinat PB',
+            'IDPel TO', 'Nama TO', 'Tarif TO', 'Daya TO', 'Tanggal Temuan',
+            'Koordinat TO', 'Keterangan TO', 'Jarak (m)'
+        ]))
+    else:
+        st.write("Tidak ada hasil yang ditemukan.")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=8888)
-    args = parser.parse_args()
-
-    start_server(app2, port=args.port)
+    main()
